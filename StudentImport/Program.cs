f4aa07f889c5c6866f181b2f07ee3c589eb39b39
Chidugowda01 <109615351+Chidugowda01@users.noreply.github.com>
2023-02-17 17:17:50 +0530

    Add files via upload

--- StudentImport/Program.cs
+++ StudentImport/Program.cs
@@ -3,7 +3,7 @@ using System.Data;
 using System.Globalization;
 using System.IO;
 using CsvHelper;
-using ExcelDataReader;
+using System;
 
 namespace StudentImport
 {
@@ -16,30 +16,35 @@ namespace StudentImport
             using (SqlConnection connection = new SqlConnection(connectionString))
             { 
                 connection.Open(); 
-                using (SqlCommand command = new SqlCommand("INSERT INTO StudentData(ExternalStudentID, FirstNmae, LastName,DOB, SSN, Addres, City, Staate, Email, MARITALSTATUS) VALUES (@ExternalStudentID, @FirstName, @LastNmae, @DOB, @SSN, @Addres, @City, @Staate, @Email, @MARITALSTATUS)", connection))
+                using (SqlCommand command = new SqlCommand("INSERT INTO StudentData(FirstName, LastName,DOB, SSN, Addres, City, Staate, Email, MARITALSTATUS) VALUES (@FirstName, @LastName, @DOB, @SSN, @Addres, @City, @Staate, @Email, @MARITALSTATUS)", connection))
                 { 
                     using (CsvReader csvReader = new CsvReader(new StreamReader(csvPath), CultureInfo.InvariantCulture))
                     { 
-                        DataTable dataTable = new DataTable();
-                        dataTable.Load((IDataReader)csvReader); 
-                        foreach (DataRow row in dataTable.Rows)
+                        using(var dr = new CsvDataReader(csvReader))
                         {
-                            command.Parameters.Clear();
-                            command.Parameters.AddWithValue("@ExternalStudentID", row["ExternalStudentID"].ToString());
-                            command.Parameters.AddWithValue("@FirstName", row["FirstName"].ToString());
-                            command.Parameters.AddWithValue("@LastName", row["LastName"].ToString());
-                            command.Parameters.AddWithValue("@DOB", row["DOB"].ToString());
-                            command.Parameters.AddWithValue("@SSN", row["SSN"].ToString());
-                            command.Parameters.AddWithValue("@Addres", row["Addres"].ToString());
-                            command.Parameters.AddWithValue("@City", row["City"].ToString());
-                            command.Parameters.AddWithValue("@Staate", row["Staate"].ToString());
-                            command.Parameters.AddWithValue("@Email", row["Email"].ToString());
-                            command.Parameters.AddWithValue("@MARITALSTATUS", row["CMARITALSTATUS"].ToString());
-                            command.ExecuteNonQuery();
+                            DataTable dataTable = new DataTable();
+                            dataTable.Load(dr);
+                            foreach (DataRow row in dataTable.Rows)
+                            {
+                                command.Parameters.Clear(); 
+                                command.Parameters.AddWithValue("@FirstName", row["FirstName"].ToString());
+                                command.Parameters.AddWithValue("@LastName", row["LastName"].ToString());
+                                command.Parameters.AddWithValue("@DOB", row["DOB"].ToString());
+                                command.Parameters.AddWithValue("@SSN", row["SSN"].ToString());
+                                command.Parameters.AddWithValue("@Addres", row["Addres"].ToString());
+                                command.Parameters.AddWithValue("@City", row["City"].ToString());
+                                command.Parameters.AddWithValue("@Staate", row["Staate"].ToString());
+                                command.Parameters.AddWithValue("@Email", row["Email"].ToString());
+                                command.Parameters.AddWithValue("@MARITALSTATUS", row["MARITALSTATUS"].ToString());
+                                
+                                command.ExecuteNonQuery();
+                            }
                         }
+                        
                     }
                 } 
                 connection.Close();
+               
             }
         }
     }
